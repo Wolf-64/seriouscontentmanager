00f5be1252012c16b1e516d0733db6ddbcfaba5a
Wolf64 <wolfbane64@outlook.com>
2025-11-02 22:50:27 +0100

    added language displace name for selector

--- src/main/java/com/wlf/app/main/data/ContentLanguage.java
+++ src/main/java/com/wlf/app/main/data/ContentLanguage.java
@@ -1,15 +1,30 @@
 package com.wlf.app.main.data;
 
+import lombok.Getter;
+
 public enum ContentLanguage {
-    DEFAULT_OR_RU,
-    UNDEFINED1,
-    UNDEFINED2,
-    UNDEFINED3,
-    EN,
-    UNDEFINED5,
-    UNDEFINED6,
-    UNDEFINED7,
-    UNDEFINED8,
-    UNDEFINED9,
-    UNDEFINED10,
+    DEFAULT_OR_RU("русский"),
+    UNDEFINED1(""),
+    UNDEFINED2(""),
+    UNDEFINED3(""),
+    EN("English"),
+    UNDEFINED5(""),
+    UNDEFINED6(""),
+    UNDEFINED7(""),
+    UNDEFINED8(""),
+    UNDEFINED9(""),
+    UNDEFINED10("");
+
+    @Getter
+    String name;
+
+    ContentLanguage(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public String toString() {
+        return getName();
+    }
 }
